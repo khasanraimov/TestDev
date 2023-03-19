50fcde5e2e0f4a6a0b80c545fc68ad609c75740c
QuattroV10 <83115762+QuattroV10@users.noreply.github.com>
2023-03-19 21:58:37 +0600

    Вот

--- FanFicApp/Main/ViewController.swift
+++ FanFicApp/Main/ViewController.swift
@@ -9,14 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func segmentOutlet(_ sender: Any) {
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let storyboard = UIStoryboard(name: "Tabbar", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "Tabbar")
         self.navigationController?.pushViewController(vc, animated: false )
+        self.view.bringSubviewToFront(loginSegmentView)
     }
 
-
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.bringSubviewToFront(loginSegmentView)
+        case 1:
+            self.view.bringSubviewToFront(registerSegmentView)
+        default:
+            break
+        }
+        
+    
+    }
+    
 }
 

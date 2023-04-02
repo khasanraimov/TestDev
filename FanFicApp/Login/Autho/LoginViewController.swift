da2fb511290c9e8f9c1acc8693a18a88fe2939a0
khasanraimov <hr.itnnes@mail.ru>
2023-04-02 22:23:10 +0300

    Test LoginViewController

---
+++ FanFicApp/Login/Autho/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  FanFicApp
+//
+//  Created by khasan on 19.03.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var segmentOutlet: UISegmentedControl!
+    
+    @IBOutlet weak var loginSegmentView: UIView!
+    
+    @IBOutlet weak var registerSegmentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
